3446a9c08eb506fd9634cb704095f28ed74d5613
marcelomora <java.diablo@gmail.com>
2021-01-21 17:18:39 -0500

    [12.0][FIX] Set 'por cobrar' in account type for domain

--- partner_credit_time_limit/models/sale.py
+++ partner_credit_time_limit/models/sale.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime
@@ -13,9 +14,14 @@ class SaleOrder(models.Model):
         self.ensure_one()
         partner = self.partner_id
         moveline_obj = self.env['account.move.line']
+        #  movelines = moveline_obj.search(
+        #      [('partner_id', '=', partner.id),
+        #       ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+        #       ('full_reconcile_id', '=', False)]
+        #  )
         movelines = moveline_obj.search(
             [('partner_id', '=', partner.id),
-             ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+             ('account_id.user_type_id.name', '=', 'Por cobrar'),
              ('full_reconcile_id', '=', False)]
         )
         today_dt = datetime.strftime(datetime.now().date(), DF)
@@ -26,12 +32,18 @@ class SaleOrder(models.Model):
             diff = today - date_maturity
             if diff.days > partner.credit_time_limit:
                 if not partner.over_credit_time_limit:
-                    msg = 'Can not confirm Sale Order, Total mature due days ' \
-                          '%s as on %s !\nCheck Partner Accounts or Credit Time ' \
-                          'Limits !' % (diff.days, today_dt)
-                    raise UserError(_('Over Customer Credit Time Limit !\n' + msg))
+                    #  msg = 'Can not confirm Sale Order, Total mature due days ' \
+                    #        '%s as on %s !\nCheck Partner Accounts or Credit Time ' \
+                    #        'Limits !' % (diff.days, today_dt)
+
+                    msg = """El cliente ha sobrepasado el tiempo de credito!
+                             No se puede confirmar la orden de venta, el total de dias vencidos
+                          {} hasta el {}!\nSolicite un pago, extienda el limite de crédito
+                          o solicite una aprobación""".format(diff.days, today_dt)
+                    raise UserError(_(msg))
         return True
 
+    @api.multi
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for order in self:
