6b8f3421a2170e7b028da4fd90c17ba04cbcba46
marcelomora <java.diablo@gmail.com>
2021-06-01 05:43:51 -0500

    [12.0][ADD] ac_product_margin. Fixing for prodcut margins in sales.

---
+++ ac_product_margin/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# Copyright 2021 Accioma (https://accioma.com).
+# @author marcelomora <java.diablo@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Margin",
+    "summary": "Product margin in products",
+    "version": " 12.0.1.0.0",
+    "category": "Sales",
+    "website": "accioma.com",
+    "author": "Accioma",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product_margin",
+    ],
+}
